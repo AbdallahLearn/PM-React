cdf41b6d7194d13d2aadcd18859132c80b2286f4
test <test@example.com>
2024-10-12 23:17:52 +0300

    branch-faraj

--- front-end/src/router/Router.jsx
+++ front-end/src/router/Router.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import Home from '../component/Home'
+import UserHome from '../component/UserHome'
+import UserProfile from '../component/UserProfile'
 const router = createBrowserRouter([
     {
         path:'/',
         element:<Home/>
-    }
+    },
+    {
+      path:'/userHome',
+      element:<UserHome />
+    },
+    {
+      path:'/userProfile',
+      element:<UserProfile/>
+    },
 ])
 function Router() {
   return (
